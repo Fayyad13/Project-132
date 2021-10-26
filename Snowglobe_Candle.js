029bcc5acf6aa021b836110cc879943c9a8f1ac2
Fayyad13 <77253413+Fayyad13@users.noreply.github.com>
2021-10-25 18:09:04 -0600

    Add files via upload

---
+++ Snowglobe_Candle.js
@@ -0,0 +1,35 @@
+status = "";
+
+function preload()
+{
+    img = loadImage("SnowGlobe_candle.jpg")
+}
+
+function setup()
+{
+    canvas = createCanvas(480, 380);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function back()
+{
+    window.location.href = "index.html";
+}
+
+function gotResult(error, results)
+{
+    if (error)
+    {
+        console.log(error);
+    }
+    console.log(results);
+}
